82a15f404f7b7866a3e1c07db9d86c1798054dd5
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-09-16 19:24:14 -0300

    dev: create readFile func

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    lista = list()
+    with open(path, mode="r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            lista.append(row)
+    return lista
